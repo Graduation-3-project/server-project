5a7bee5afa58c9eb96983ad7b9b008ec46f4a732
i55 <i@DESKTOP-G7TCLEJ>
2020-05-05 13:34:25 +0800

    统计表

--- GraduationProject/graduation-project-web/src/main/java/gp/lcw/sd/by/g3p/extension/rest/locationControlller.java
+++ GraduationProject/graduation-project-web/src/main/java/gp/lcw/sd/by/g3p/extension/rest/locationControlller.java
@@ -5,18 +5,30 @@ import gp.lcw.sd.by.g3p.extension.dao.location;
 import gp.lcw.sd.by.g3p.extension.domain.locationDaoOperate;
 import gp.lcw.sd.by.g3p.extension.serviceManager.locationManager;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @RestController
 @RequestMapping("/locatin")
 public class locationControlller extends GenericController<location,Long, locationManager> {
 
-
-
     @Autowired
     locationDaoOperate locationDaoOperate;
+
+
+
+    @GetMapping("/findAll")
+    @ResponseBody
+    public List<location>locationList(){
+        List<location>locationList=locationDaoOperate.findAll();
+        return  locationList;
+    }
+
 
 
     //public location location=new location();
